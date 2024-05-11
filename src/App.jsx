f4f30e37a6eaf7255bf62f5a6f6123676006ca72
@Cesarscc Cesarscc <ccoloradoc@uni.pe>
2024-05-10 20:11:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,98 +6,220 @@ function App() {
   const [operation, setOperation] = useState("0");
 
   const handleChangeOperation = (op) => {
-    if (
-      !(
-        result[result.length - 1] == "+" ||
-        result[result.length - 1] == "-" ||
-        result[result.length - 1] == "/" ||
-        result[result.length - 1] == "·"
-      ) ||
-      !(op == "X" || op == "/" || op == "+" || op == "-")
-    ) {
-      //Verificamos que no se ingrese doble operacion seguida
-      if (!operation.includes(".") || op != ".") {
-        //verificamos que no se puedan ingresar 2 puntos decimales
-        if (op == "=") {
-          //si es el operador resultado
-          if (
+    if (op === "0") {
+      if (operation.length !== 1 && operation !== "0") {
+        if (
+          !(
             result[result.length - 1] == "+" ||
             result[result.length - 1] == "-" ||
             result[result.length - 1] == "/" ||
             result[result.length - 1] == "·"
-          ) {
-            //Verificamos que el ultimo a operar sea un operador, si es así lo sacamos de la operación
-            setResult((prevSate) => prevSate.slice(0, -1)); //lo sacamos de la operación el último operando
-            const formatRpta = result.slice(0, -1).replace("·", "*");
-            const rpta = eval(formatRpta).toFixed(10); //realizamos la operacion con el ultimo operador ya sacado
-            setResult((prevSate) => prevSate + "=" + rpta);
-            setOperation(rpta);
-          } else {
-            //SI NO ES EL OPERADOR RESULTADO
-            if (result.length == 0) {
-              //si no hay nada que operar
-              setResult("=NaN");
-              setOperation("NaN");
-            } else {
-              //Si hay algo que operar
-              const formatRpta = result.replace(/·/g, "*"); //cambiamos a multiplicar
-              if (eval(formatRpta).toString().indexOf(".") !== -1) {
-                //vERIFICAMOS SI TIENE DECIMALES
-                const rpta = eval(formatRpta).toFixed(10);
+          ) ||
+          !(op == "X" || op == "/" || op == "+" || op == "-")
+        ) {
+          //Verificamos que no se ingrese doble operacion seguida
+          if (!operation.includes(".") || op != ".") {
+            //verificamos que no se puedan ingresar 2 puntos decimales
+            if (op == "=") {
+              //si es el operador resultado
+              if (
+                result[result.length - 1] == "+" ||
+                result[result.length - 1] == "-" ||
+                result[result.length - 1] == "/" ||
+                result[result.length - 1] == "·"
+              ) {
+                //Verificamos que el ultimo a operar sea un operador, si es así lo sacamos de la operación
+                setResult((prevSate) => prevSate.slice(0, -1)); //lo sacamos de la operación el último operando
+                const formatRpta = result.slice(0, -1).replace("·", "*");
+                const rpta = eval(formatRpta).toFixed(10); //realizamos la operacion con el ultimo operador ya sacado
                 setResult((prevSate) => prevSate + "=" + rpta);
                 setOperation(rpta);
               } else {
-                const rpta = eval(formatRpta).toString();
-                setResult((prevSate) => prevSate + "=" + rpta);
-                setOperation(rpta);
+                //SI NO ES EL OPERADOR RESULTADO
+                if (result.length == 0) {
+                  //si no hay nada que operar
+                  setResult("=NaN");
+                  setOperation("NaN");
+                } else {
+                  //Si hay algo que operar
+                  const formatRpta = result.replace(/·/g, "*"); //cambiamos a multiplicar
+                  if (eval(formatRpta).toString().indexOf(".") !== -1) {
+                    //vERIFICAMOS SI TIENE DECIMALES
+                    const rpta = eval(formatRpta).toFixed(10);
+                    setResult((prevSate) => prevSate + "=" + rpta);
+                    setOperation(rpta);
+                  } else {
+                    const rpta = eval(formatRpta).toString();
+                    setResult((prevSate) => prevSate + "=" + rpta);
+                    setOperation(rpta);
+                  }
+                }
+              }
+            } else {
+              if (result.includes("=")) {
+                if (op == "+" || op == "/" || op == "-" || op == "X") {
+                  setResult(operation);
+                } else {
+                  setResult("");
+                  setOperation("");
+                }
+              }
+              if (result.includes("Infinity")) {
+                setResult("");
+              }
+              if (result.length > 0) {
+                //SI LA CADENA ES MAYOR QUE 0
+                if (op == "+" || op == "/" || op == "-" || op == "X") {
+                  setOperation(op); //SI ES ALGUNA OPERACION
+                } else if (
+                  //Si donde se realiza la operacion tiene un operador
+                  operation == "+" ||
+                  operation == "/" ||
+                  operation == "-" ||
+                  operation == "X"
+                ) {
+                  setOperation("");
+                  setOperation((prevState) => prevState + op);
+                } else {
+                  setOperation(
+                    (prevState) =>
+                      prevState.replace("NaN", "").replace("Infinity", "") + op
+                  );
+                }
+              } else {
+                setOperation(op);
+              }
+              if (op == "X") {
+                setResult((prevSate) => prevSate + "·");
+              } else if (
+                op == "." &&
+                (result[result.length - 1] == "+" ||
+                  result[result.length - 1] == "-" ||
+                  result[result.length - 1] == "·" ||
+                  result[result.length - 1] == "/")
+              ) {
+                setResult((prevSate) => prevSate + "0.");
+                setOperation("0.");
+              } else if (op == "." && result.length == 0) {
+                setResult("0.");
+                setOperation("0.");
+              } else {
+                setResult((prevSate) => prevSate + op);
               }
             }
-          }
-        } else {
-          if (result.includes("=")) {
-            if (op == "+" || op == "/" || op == "-" || op == "X") {
-              setResult(operation);
-            } else {
-              setResult("");
+          } else if (result.length > 0) {
+            if (result.includes("=")) {
+              setResult("0.");
+              setOperation("0.");
             }
-          }
-          if (result.includes("Infinity")) {
-            setResult("");
-          }
-          if (result.length > 0) {
-            //SI LA CADENA ES MAYOR QUE 0
-            if (op == "+" || op == "/" || op == "-" || op == "X") {
-              setOperation(op); //SI ES ALGUNA OPERACION
-            } else if (
-              //Si donde se realiza la operacion tiene un operador
-              operation == "+" ||
-              operation == "/" ||
-              operation == "-" ||
-              operation == "X"
-            ) {
-              setOperation("");
-              setOperation((prevState) => prevState + op);
-            } else {
-              setOperation(
-                (prevState) =>
-                  prevState.replace("NaN", "").replace("Infinity", "") + op
-              );
-            }
-          } else {
-            setOperation(op);
-          }
-          if (op == "X") {
-            setResult((prevSate) => prevSate + "·");
-          } else if (op == "." && result.length == 0) {
-            setResult("0.");
-          } else {
-            setResult((prevSate) => prevSate + op);
           }
         }
-      } else if (result.length > 0) {
-        if (result.includes("=")) {
-          setResult("0.");
-          setOperation("0.");
+      }
+    } else {
+      if (
+        !(
+          result[result.length - 1] == "+" ||
+          result[result.length - 1] == "-" ||
+          result[result.length - 1] == "/" ||
+          result[result.length - 1] == "·"
+        ) ||
+        !(op == "X" || op == "/" || op == "+" || op == "-")
+      ) {
+        //Verificamos que no se ingrese doble operacion seguida
+        if (!operation.includes(".") || op != ".") {
+          //verificamos que no se puedan ingresar 2 puntos decimales
+          if (op == "=") {
+            //si es el operador resultado
+            if (
+              result[result.length - 1] == "+" ||
+              result[result.length - 1] == "-" ||
+              result[result.length - 1] == "/" ||
+              result[result.length - 1] == "·"
+            ) {
+              //Verificamos que el ultimo a operar sea un operador, si es así lo sacamos de la operación
+              setResult((prevSate) => prevSate.slice(0, -1)); //lo sacamos de la operación el último operando
+              const formatRpta = result.slice(0, -1).replace("·", "*");
+              const rpta = eval(formatRpta).toFixed(10); //realizamos la operacion con el ultimo operador ya sacado
+              setResult((prevSate) => prevSate + "=" + rpta);
+              setOperation(rpta);
+            } else {
+              //SI NO ES EL OPERADOR RESULTADO
+              if (result.length == 0) {
+                //si no hay nada que operar
+                setResult("=NaN");
+                setOperation("NaN");
+              } else {
+                //Si hay algo que operar
+                const formatRpta = result.replace(/·/g, "*"); //cambiamos a multiplicar
+                if (eval(formatRpta).toString().indexOf(".") !== -1) {
+                  //vERIFICAMOS SI TIENE DECIMALES
+                  const rpta = eval(formatRpta).toFixed(10);
+                  setResult((prevSate) => prevSate + "=" + rpta);
+                  setOperation(rpta);
+                } else {
+                  const rpta = eval(formatRpta).toString();
+                  setResult((prevSate) => prevSate + "=" + rpta);
+                  setOperation(rpta);
+                }
+              }
+            }
+          } else {
+            if (result.includes("=")) {
+              if (op == "+" || op == "/" || op == "-" || op == "X") {
+                setResult(operation);
+              } else {
+                setResult("");
+                setOperation("");
+              }
+            }
+            if (result.includes("Infinity")) {
+              setResult("");
+            }
+            if (result.length > 0) {
+              //SI LA CADENA ES MAYOR QUE 0
+              if (op == "+" || op == "/" || op == "-" || op == "X") {
+                setOperation(op); //SI ES ALGUNA OPERACION
+              } else if (
+                //Si donde se realiza la operacion tiene un operador
+                operation == "+" ||
+                operation == "/" ||
+                operation == "-" ||
+                operation == "X"
+              ) {
+                setOperation("");
+                setOperation((prevState) => prevState + op);
+              } else {
+                setOperation(
+                  (prevState) =>
+                    prevState.replace("NaN", "").replace("Infinity", "") + op
+                );
+              }
+            } else {
+              setOperation(op);
+            }
+            if (op == "X") {
+              setResult((prevSate) => prevSate + "·");
+            } else if (
+              op == "." &&
+              (result[result.length - 1] == "+" ||
+                result[result.length - 1] == "-" ||
+                result[result.length - 1] == "·" ||
+                result[result.length - 1] == "/")
+            ) {
+              setResult((prevSate) => prevSate + "0.");
+              setOperation("0.");
+            } else if (op == "." && result.length == 0) {
+              setResult("0.");
+              setOperation("0.");
+            } else {
+              setResult((prevSate) => prevSate + op);
+            }
+          }
+        } else if (result.length > 0) {
+          if (result.includes("=")) {
+            setResult("0.");
+            setOperation("0.");
+          }
         }
       }
     }
